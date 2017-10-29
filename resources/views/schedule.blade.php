83035a53c263c42296149dbb469321738bb13d37
tonfonka <phuritchaya.1234@mail.kmutt.ac.th>
2017-10-29 21:32:39 +0700

    graph

--- resources/views/schedule.blade.php
+++ resources/views/schedule.blade.php
@@ -11,6 +11,30 @@
     <link href="/css/uptotrain2.min.css" rel="stylesheet">
    
     <link href="https://fonts.googleapis.com/css?family=Prompt" rel="stylesheet">
+
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load("current", {packages:["corechart"]});
+      google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+          ['Work',     11],
+          ['Eat',      2],
+          ['Commute',  2],
+          ['Watch TV', 2],
+          ['Sleep',    7]
+        ]);
+
+        var options = {
+          title: 'My Daily Activities',
+          pieHole: 0.4,
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+        chart.draw(data, options);
+      }
+    </script>
 </head>
 <body id="page-top" class="index">
     <div align="right">
@@ -163,6 +187,7 @@
 </div><!-- /col-sm-12 -->
 </div><!-- /row -->
 <div class="row">
+<div id="donutchart" style="width: 900px; height: 500px;"></div>
 @foreach($review as $reviews)
 <?php
     $userName = DB::table('users')->where('id',$reviews->user_id)->get();
