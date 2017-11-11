2857837aecc9893a8af6c960230e37936899eb53
supichhmint <supichaya02@gmail.com>
2017-11-12 04:01:35 +0700

    edit admin_index

--- resources/views/admin/admin_index.blade.php
+++ resources/views/admin/admin_index.blade.php
@@ -16,7 +16,7 @@
                             <i class="fa fa-comment fa-5x"></i>
                         </div>
                         <div class="col-xs-9 text-right">
-                            <div class="huge">12</div>
+                            <div class="huge">{{$countcontact}}</div>
                             <div>New Message!</div>
                         </div>
                     </div>
@@ -33,53 +33,16 @@
             </div>
         </div>
 
-
-        <div id="page-wrapper">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h1 class="page-header">Dashboard</h1>
-                </div>
-                <!-- /.col-lg-12 -->
-            </div>
-            <!-- /.row -->
-            <div class="row">
-                
-                <div class="col-lg-6 col-md-6">
-                    <div class="panel panel-green">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <i class="fa fa-comment fa-5x"></i>
-                                </div>
-                                <div class="col-xs-9 text-right">
-                                    <div class="huge">{{$countcontact}}</div>
-                                    <div>New Message!</div>
-                                </div>
-                            </div>
+        <div class="col-lg-6 col-md-6">
+            <div class="panel panel-red">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-xs-3">
+                            <i class="fa fa-legal fa-5x"></i>
                         </div>
-                        <a href="#">
-                            <div class="panel-footer">
-                                <span class="pull-left">View Details</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-               
-                <div class="col-lg-6 col-md-6">
-                    <div class="panel panel-red">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <i class="fa fa-legal fa-5x"></i>
-                                </div>
-                                <div class="col-xs-9 text-right">
-                                    <div class="huge">{{$countagency}}</div>
-                                    <div>New Appore!</div>
-                                </div>
-                            </div>
-
+                        <div class="col-xs-9 text-right">
+                            <div class="huge">{{$countagency}}</div>
+                            <div>New Appore!</div>
                         </div>
                     </div>
                 </div>
